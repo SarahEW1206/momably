d08d1fefa1a41a9b007a6631b7c0898c5134cef3
Sarah Waldie <sarah@sassafrasbiz.com>
2019-04-03 14:18:30 -0400

    Updates

---
+++ precache-manifest.0bbe32add129b4716f90989824497c31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b15800fcdda5b13b291",
+    "url": "/static/css/main.c689209c.chunk.css"
+  },
+  {
+    "revision": "5b15800fcdda5b13b291",
+    "url": "/static/js/main.5b15800f.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "8f046f4e16d0fc40318a",
+    "url": "/static/js/2.8f046f4e.chunk.js"
+  },
+  {
+    "revision": "d67cc911f4f7e65b9704e3875d9f52ef",
+    "url": "/static/media/momably-home-collab.d67cc911.jpg"
+  },
+  {
+    "revision": "123d573975e9b5db61e7a1e1cef173bd",
+    "url": "/index.html"
+  }
+];
