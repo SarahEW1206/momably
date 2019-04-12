8e429881363700a5723f2d4c737dfd4b97d4c499
Sarah Waldie <sarah@sassafrasbiz.com>
2019-04-12 11:22:54 -0400

    Updates

---
+++ precache-manifest.6b247da1cf940ff679f6185ea015a6fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0705431b0ff37cb3ca6",
+    "url": "/static/css/main.3d477747.chunk.css"
+  },
+  {
+    "revision": "d0705431b0ff37cb3ca6",
+    "url": "/static/js/main.d0705431.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "7936845144cf3f5e7b2a",
+    "url": "/static/js/2.79368451.chunk.js"
+  },
+  {
+    "revision": "d67cc911f4f7e65b9704e3875d9f52ef",
+    "url": "/static/media/momably-home-collab.d67cc911.jpg"
+  },
+  {
+    "revision": "8e5949502ebd46feb103a822c28d75c4",
+    "url": "/static/media/mom-and-lincoln2.8e594950.jpg"
+  },
+  {
+    "revision": "079f672db17ff54d90c770da291712cf",
+    "url": "/index.html"
+  }
+];
